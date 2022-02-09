ff783f8e45275aeddfcd647f6d96138177d9fba2
Gideon ojo <ojoGideon5@gmail.com>
2022-02-09 18:31:10 +0100

    Update CarRestController.java

--- src/main/java/com/carservice/application/restcontrollers/CarRestController.java
+++ src/main/java/com/carservice/application/restcontrollers/CarRestController.java
@@ -23,6 +23,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
